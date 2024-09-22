ed4087225b711acc25085c2d5f7fce251504a5b6
tinodewa <dtino0812@gmail.com>
2024-09-22 14:35:25 +0700

    separate connection and controller function

---
+++ controllers/setupDB.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoDB Singleton Struct
+// type mongoClient struct {
+// 	client *mongo.Client
+// 	ctx    context.Context
+// 	once   sync.Once
+// }
+
+// var mongoInstance mongoClient
+
+// Connect app to mongoDB atlas
+func GetMongoClient() (*mongo.Client, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	mongoUri := os.Getenv("MONGO_URI")
+
+	// Connect to your atlas cluster:
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		mongoUri,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	// defer func() {
+	// 	if err = client.Disconnect(ctx); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println("There was a problem connectiong to your Atlas cluster. Check that the URI includes a valid username and password, and that your IP address has been added to the access list. Error: ")
+		panic(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	return client, nil
+}
--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"context"
 	"hit/album-mongo-api/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
 // A Album Store Resource allows you to insert album into collection
 
 func main() {
-	controllers.StoreAndGetAlbum()
+	// Instantiate Gin
+	router := gin.Default()
+
+	// Connect to MongoDB
+	client, err := controllers.GetMongoClient()
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	albums, err := controllers.GetAlbums(context.Background(), client)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, album := range albums {
+		println(album.Title)
+	}
+
+	// Create a route with GET method
+	router.GET("/", func(ctx *gin.Context) {
+		// Return JSON response
+		ctx.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	// Create a route to get all albums
+	router.GET("/api/albums", controllers.GetAlbums)
+
+	// Create server with port 3000
+	router.Run(":3000")
+
 }
